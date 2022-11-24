e18618fadffc8fd62664385dedda562c105c2072
Rikowahyudi <115127472+Rikowahyudi@users.noreply.github.com>
2022-11-24 13:11:40 +0700

    Create Variabel.java

---
+++ Variabel.java
@@ -0,0 +1,12 @@
+public class UTS {
+  public static void main(String[] args) {
+  String salam = "Assalamu'alaikum";
+  String nama = "Riko Wahyudi";
+  int tahun_lahir = 2004;
+  
+  System.out.println(salam);
+  System.out.println("Nama Saya : " + nama);
+  System.out.println("Saya Lahir Di Tahun : " + tahun_lahir);
+    
+  }
+}
